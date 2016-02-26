d27ea684e41e6aa79f98f1e91e112b72ae6645d1
Tom Quist <tom@quist.de>
2016-02-26 19:39:28 +0100

    Added wrapper validators to allow logical arithmetic And/Or/Not

--- ValidatedTests/ValidatedTests.swift
+++ ValidatedTests/ValidatedTests.swift
@@ -19,7 +19,7 @@ class ValidatedTests: XCTestCase {
 
     func testValidatesEmptyString() {
         // Define a type for a non-empty string
-        typealias NonEmptyString = Validated<String, NonEmptyStringValidator>
+        typealias NonEmptyString = Validated<String, Not<EmptyStringValidator>>
         // Create an empty string
         let valueNotValidated = NonEmptyString("")?.value
         XCTAssertNil(valueNotValidated)
@@ -31,7 +31,7 @@ class ValidatedTests: XCTestCase {
 
     func testValidatesAnyEmptyCollection() {
         // Define a type for a non-empty collection
-        typealias NonEmptyListOfStrings = Validated<[String], NonEmptyCollectionValidator<[String]>>
+        typealias NonEmptyListOfStrings = Validated<[String], Not<EmptyCollectionValidator<[String]>>>
         // Create an empty list of strings
         let valueNotValidated = NonEmptyListOfStrings([])?.value
         XCTAssertNil(valueNotValidated)
@@ -83,10 +83,10 @@ class ValidatedTests: XCTestCase {
 
     // MARK: Validate2 Tests
 
-    func testValidatesEmptyStringAndAllCaps() {
+    func testValidatesEmptyStringAndAllCaps1() {
         // Define a type for a non-empty all caps latin string
         typealias AllCapsNonEmptyString =
-            Validated2<String, NonEmptyStringValidator, AllCapsLatinStringValidator>
+            Validated2<String, Not<EmptyStringValidator>, AllCapsLatinStringValidator>
 
         // Create an empty string
         let valueNotValidated = AllCapsNonEmptyString("")?.value
@@ -123,7 +123,7 @@ class ValidatedTests: XCTestCase {
 
     func testValidatesEmptyStringAndAllCapsContainsYorZ() {
         typealias AllCapsNonEmptyStringWithYorZ =
-            Validated3<String, NonEmptyStringValidator, AllCapsLatinStringValidator, ContainsYorZ>
+            Validated3<String, Not<EmptyStringValidator>, AllCapsLatinStringValidator, ContainsYorZ>
 
         // Create a non-complying string
         let valueNotValidated = AllCapsNonEmptyStringWithYorZ("ABCDEF")?.value
@@ -132,6 +132,58 @@ class ValidatedTests: XCTestCase {
         // Create a compying string
         let valueValidated = AllCapsNonEmptyStringWithYorZ("ABCDEFY")?.value
         XCTAssertEqual(valueValidated, "ABCDEFY")
+    }
+    
+    // MARK: Validator And/Or/Not Tests
+
+    func testValidatesEmptyStringAndAllCaps2() {
+        // Define a type for a non-empty all caps latin string
+        typealias AllCapsNonEmptyString =
+            Validated<String, And<Not<EmptyStringValidator>, AllCapsLatinStringValidator>>
+
+        // Create an empty string
+        let valueNotValidated = AllCapsNonEmptyString("")?.value
+        XCTAssertNil(valueNotValidated)
+
+        // Create a string with lowercase letters
+        let valueNotValidated2 = AllCapsNonEmptyString("xYZcF")?.value
+        XCTAssertNil(valueNotValidated2)
+
+        // Create an all caps string
+        let valueValidated = AllCapsNonEmptyString("XYZCF")?.value
+        XCTAssertEqual(valueValidated, "XYZCF")
+    }
+    
+    func testValidatesEmptyStringOrAllCaps() {
+        // Define a type for an empty or all caps latin string
+        typealias AllCapsOrEmptyString =
+            Validated<String, Or<EmptyStringValidator, AllCapsLatinStringValidator>>
+        
+        // Create an empty string
+        let valueValidated1 = AllCapsOrEmptyString("")?.value
+        XCTAssertEqual(valueValidated1, "")
+        
+        // Create a string with lowercase letters
+        let valueNotValidated = AllCapsOrEmptyString("xYZcF")?.value
+        XCTAssertNil(valueNotValidated)
+        
+        // Create an all caps string
+        let valueValidated2 = AllCapsOrEmptyString("XYZCF")?.value
+        XCTAssertEqual(valueValidated2, "XYZCF")
+    }
+    
+    func testValidatesNonEmpty() {
+        // Define a type for a non-empty string
+        typealias AllCapsNonEmptyString =
+            Validated<String, Not<EmptyStringValidator>>
+        
+        // Create an empty string
+        let valueNotValidated = AllCapsNonEmptyString("")?.value
+        XCTAssertNil(valueNotValidated)
+        
+        // Create an all caps string
+        let valueValidated = AllCapsNonEmptyString("abC")?.value
+        XCTAssertEqual(valueValidated, "abC")
     }
 
 }
